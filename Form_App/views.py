622d37ae5ddb8f1187ae069bf2b2b015dc703909
priyanka <neuboticsitpriyanka@gmail.com>
2022-04-18 10:54:14 +0530

    update gallery section button and add dropdown in registration form also update Form_App view & urls

--- Form_App/views.py
+++ Form_App/views.py
@@ -16,9 +16,9 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required()
-def home(request):
-    return render(request, 'Form_App/home.html')
+# @login_required()
+# def home(request):
+#     return render(request, 'Form_App/home.html')
 
 
 def register(request, num='Standard'):
@@ -32,10 +32,10 @@ def register(request, num='Standard'):
         try:
             if User.objects.filter(username=username).first():
                 messages.error(request, 'username already exist')
-                return redirect('/register/<str:num>/')
+                return redirect('/register')
             if User.objects.filter(email=email).first():
                 messages.error(request, 'email already exist')
-                return redirect('/register/<str:num>/')
+                return redirect('/register')
             user_obj = User.objects.create(username=username, email=email)
             user_obj.set_password(password)
             user_obj.save()
@@ -44,76 +44,78 @@ def register(request, num='Standard'):
             profile_obj = profile.objects.create(user=user_obj, auth_token=auth_token,price=price,contact=contact)
             profile_obj.save()
             send_email_after_register(email, auth_token)
-            return redirect('/token_send')
+            messages.success(request, 'Please check Your Email')
+            return redirect('/register')
+            # return redirect('/token_send')
         except Exception as e:
             print(e)
 
     return render(request, 'Form_App/register.html',{'num' : num})
 
 
-def login(request):
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+# def login(request):
+#         if request.method == 'POST':
+#             username = request.POST.get('username')
+#             password = request.POST.get('password')
 
-            user_obj = User.objects.filter(username=username).first()
-            if user_obj is None:
-                messages.success(request, 'user not found')
-                return redirect('/login')
+#             user_obj = User.objects.filter(username=username).first()
+#             if user_obj is None:
+#                 messages.success(request, 'user not found')
+#                 return redirect('/login')
 
-            profile_obj = profile.objects.filter(user=user_obj).first()
-            if not profile_obj.is_verified:
-                messages.success(
-                    request, 'Profile is not verify check your mail')
-                return redirect('/login')
+#             profile_obj = profile.objects.filter(user=user_obj).first()
+#             if not profile_obj.is_verified:
+#                 messages.success(
+#                     request, 'Profile is not verify check your mail')
+#                 return redirect('/login')
 
-            user = authenticate(username=username, password=password)
-            if user is None:
-                messages.success(request, 'Wrong password')
-                return redirect('/login')
+#             user = authenticate(username=username, password=password)
+#             if user is None:
+#                 messages.success(request, 'Wrong password')
+#                 return redirect('/login')
 
-            auth_login(request, user)
-            return redirect('/home')
+#             auth_login(request, user)
+#             return redirect('/home')
 
-        return render(request, 'Form_App/login.html')
-
-
-def success(request):
-    return render(request, 'Form_App/success.html')
+#         return render(request, 'Form_App/login.html')
 
 
-def token_send(request):
-    return render(request, 'Form_App/token_send.html')
+# def success(request):
+#     return render(request, 'Form_App/success.html')
+
+
+# def token_send(request):
+#     return render(request, 'Form_App/token_send.html')
 
 
 def send_email_after_register(email, token):
     subject = 'Your Account need to be verified'
-    message = f'Your Username And Password Will be Generate Within 24 Hour. Hi paste the link to verify your account - {token}. '
+    message = f'Your Username And Password Will be Generate Within 24 Hour. Your token number is - {token}. '
     # message = f' Hi paste the link to verify your account http://127.0.0.1:8000/verify/{token}. '
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
 
 
-def verify(request, auth_token):
-    try:
-        profile_obj = profile.objects.filter(auth_token=auth_token).first()
+# def verify(request, auth_token):
+#     try:
+#         profile_obj = profile.objects.filter(auth_token=auth_token).first()
 
-        if profile_obj.is_verified:
-            messages.success(request, 'Your account is already verified')
-            return redirect('/login')
+#         if profile_obj.is_verified:
+#             messages.success(request, 'Your account is already verified')
+#             return redirect('/login')
 
-        if profile_obj:
-            profile_obj.is_verified = True
-            profile_obj.save()
-            messages.success(request, 'Your account has been verified')
-            return redirect('/login')
-        else:
-            return redirect('/error')
-    except Exception as e:
-           print(e)
-           return redirect('/home')
+#         if profile_obj:
+#             profile_obj.is_verified = True
+#             profile_obj.save()
+#             messages.success(request, 'Your account has been verified')
+#             return redirect('/login')
+#         else:
+#             return redirect('/error')
+#     except Exception as e:
+#            print(e)
+#            return redirect('/home')
 
 
-def error(request):
-    return render(request, 'Form_App/error.html')
+# def error(request):
+#     return render(request, 'Form_App/error.html')
